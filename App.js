7d77c7fe7fa04be4ad31adbfe798480b01c400ee
handrkanda <handrykanda@gmail.com>
2020-06-13 10:59:01 +0200

    Resolved dependency versions

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import 'react-native-gesture-handler';
 
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { Text, StyleSheet, View } from 'react-native'
+import {Text, StyleSheet, View} from 'react-native';
 
 //redux stuff
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './src/redux/store';
 
 //screens
-import TabScreen from './src/screens/TabScreen'
+import TabScreen from './src/screens/TabScreen';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <NavigationContainer>
-          <TabScreen/>
+          <TabScreen />
         </NavigationContainer>
-      </Provider>  
-    )
+      </Provider>
+    );
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
